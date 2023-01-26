7b0f0cd615bd3222f0a0c0af66aac1e9d72d60a0
mar bautista <marilirulita@gmail.com>
2023-01-25 18:43:11 -0800

    add setup files

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_many :reviews
+
+  before_create :slugify
+
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    reviews.average(:score).to_f.round(2)
+  end
+end
