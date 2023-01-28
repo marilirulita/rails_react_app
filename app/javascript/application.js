a4187f3c35cdffdba446365c58383800b025cb6e
mar bautista <marilirulita@gmail.com>
2023-01-27 16:02:17 -0800

    restore app js pplication file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+import "@hotwired/turbo-rails"
+import "controllers"
