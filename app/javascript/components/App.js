953a606ff91d90e57048873ca0bad5f6a20b518e
mar bautista <marilirulita@gmail.com>
2023-01-28 16:23:20 -0800

    add first config react

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './User';
+
+function App() {
+  return (<main>
+      <h1>Rails Todo Playground</h1>
+      <div>
+      <User />
+      </div>
+    </main>);
+}
+
+export default App;
